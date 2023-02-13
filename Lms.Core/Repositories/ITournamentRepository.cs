13626f2568471f29935d8dc74e815c7a7aaffae6
soroshdotnet <sorosh.farahmand@gmail.com>
2023-02-13 15:24:56 +0100

    ITournamentRepository added

---
+++ Lms.Core/Repositories/ITournamentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Lms.Core.Enteties;
+
+namespace Lms.Core.Repositories
+{
+    public interface ITournamentRepository
+    {
+        Task<IEnumerable<Tournament>> GetAllAsync(); 
+        Task<Tournament> GetAsync(int id);
+        Task<bool> AnyAsync(int id);
+        void Add(Tournament tournament); 
+        void Update(Tournament tournament); 
+        void Remove(Tournament tournament);
+    }
+}
